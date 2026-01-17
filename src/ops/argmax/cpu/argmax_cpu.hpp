52eda22a02b01699bcf8ecf442dd14e15835ad94
vankari <760238843@qq.com>
2026-01-17 18:31:35 +0800

    correct hw2 1

--- src/ops/argmax/cpu/argmax_cpu.hpp
+++ src/ops/argmax/cpu/argmax_cpu.hpp
@@ -4,5 +4,5 @@
 #include <cstddef>
 
 namespace llaisys::ops::cpu {
-void argmax(std::byte *mi, const std::byte *mv, const std::byte *v, llaisysDataType_t type, size_t size);
+void argmax(std::byte *mi, std::byte *mv, const std::byte *v, llaisysDataType_t type, size_t size);
 }
