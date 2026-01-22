fcd281c7763af1f6d48e1cfcc43ba3298972eba6
vankari <760238843@qq.com>
2026-01-22 14:58:11 +0800

    修下hw1 bug

--- src/tensor/tensor.cpp
+++ src/tensor/tensor.cpp
@@ -171,7 +171,7 @@ bool Tensor::isContiguous() const {
     }    
       
     for (size_t i = 0; i < shape.size(); ++i) {    
-        size_t expected_stride = 1;    
+        auto expected_stride = 1;    
         for (size_t j = i + 1; j < shape.size(); ++j) {    
             expected_stride *= shape[j];    
         }    
